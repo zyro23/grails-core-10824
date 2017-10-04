518df20e1c0aa2f6e9e04265cd59850ccbac10bb
zyro <zyro@zyro.net>
2017-10-04 10:13:58 +0200

    sample app reproducing grails/grails-core#10824

--- grails-app/init/myapp/BootStrap.groovy
+++ grails-app/init/myapp/BootStrap.groovy
@@ -1,8 +1,14 @@
 package myapp
 
+import org.grails.transaction.ChainedTransactionManager
+import org.springframework.transaction.PlatformTransactionManager
+
 class BootStrap {
 
+    PlatformTransactionManager transactionManager
+
     def init = { servletContext ->
+        assert transactionManager in ChainedTransactionManager
     }
     def destroy = {
     }
